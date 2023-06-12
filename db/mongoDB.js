4d440ffe3f068e54d24bbd1295749bc48259c0e7
Q300Z <34000576+Q300Z@users.noreply.github.com>
2023-06-12 19:01:23 +0200

    Mise à jour de l'ip de la base de donnée

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const db = "API-FAC"; // remplacer avec le nom de votre base de données
-const uri = `mongodb://127.0.0.1:27017/${db}`; // remplacer avec l'URI de votre base de données MongoDB
+const uri = `mongodb://192.168.1.100:27017/${db}`; // remplacer avec l'URI de votre base de données MongoDB
 
 async function connectDB() {
   try {
